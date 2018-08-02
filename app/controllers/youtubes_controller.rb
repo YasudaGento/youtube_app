33b8c4974149109143b673a2e3503ac619b62b5a
Yasuda Gento <gennto19930406@gmail.com>
2018-08-02 01:37:03 +0000

    chenged youtube

--- app/controllers/youtubes_controller.rb
+++ app/controllers/youtubes_controller.rb
@@ -3,7 +3,36 @@ class YoutubesController < ApplicationController
   
   def index
    #get_data("world+of+dance")
+   #require 'youtube.rb'#先ほど上で準備したファイルを呼ぶ
+   opts = Trollop::options do
+     opt :q, 'Search term', :type => String, :default => "world+of+dance"
+     opt :max_results, 'Max results', :type => :int, :default => 50
+     opt :order, 'order', :type => String, :default => 'date'
+     opt :regionCode, 'region', :type => String, :default => 'JP'
+    
+  end
+
+  client,youtube=get_service
+
+  begin
+
+    search_response = client.execute!(
+      :api_method => youtube.search.list,
+      :parameters => {
+        :part => 'snippet',
+        :q => opts[:q],
+        :maxResults => opts[:max_results],
+        :order => opts[:order],
+        :regionCode => opts[:regionCode]
+      }
+    )
+
+  @videos= search_response.data.items  #Jsonの中身が多かったので必要な情報のみ受けれるようにしています。
+  #セーブを書き込む
   
+  rescue Google::APIClient::TransmissionError => e
+    puts e.result.body
+  end
   end
   
   def show
